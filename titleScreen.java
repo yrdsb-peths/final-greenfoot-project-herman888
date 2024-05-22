cdec8a3b8efc5aa1b517b3e54a3a4545a48067de
herman888 <341140944@gapps.yrdsb.ca>
2024-05-22 18:45:36 -0400

    Made  A Title Screen

--- titleScreen.java
+++ titleScreen.java
@@ -17,5 +17,23 @@ public class titleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Hero hero = new Hero();
+        addObject(hero,450,275);
+        
+        
+        Label titleLabel = new Label("Space Invaders",70);
+        addObject(titleLabel,320,50);
+        
+        Label continueLabel = new Label("Press <space> to Play", 50);
+        addObject(continueLabel,300,250);
     }
 }
